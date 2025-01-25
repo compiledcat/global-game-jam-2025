e25d5876c1270d69c5593e9227ca09c1b86f2e30
Thomas Jowsey <2302820@uad.ac.uk>
2025-01-25 16:04:47 +0000

    improve lobby UI

--- Assets/Scripts/LobbyPlayerBox.cs
+++ Assets/Scripts/LobbyPlayerBox.cs
@@ -1,6 +1,11 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class LobbyPlayerBox : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI _playerNameText;
+    [SerializeField] private TextMeshProUGUI _playerIndexText;
+
+    public void SetPlayerName(string playerName) => _playerNameText.text = playerName;
+    public void SetPlayerIndex(int index) => _playerIndexText.text = index.ToString();
 }
