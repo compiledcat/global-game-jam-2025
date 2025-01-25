dfd9af4b3c2e2ecaf8cbd11fbd40d76955ed04da
Serpendio <58090288+Serpendio@users.noreply.github.com>
2025-01-25 23:06:17 +0000

    Bubbles & Track (#12)
    Bubbles shader and logic
    Track - generate bubbles & checkpoints

---
+++ Assets/Scripts/BubbleDetect.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class BubbleDetect : MonoBehaviour
+{
+    [SerializeField] Vector2 bounds;
+    [SerializeField] float height, offset, scale, sizeVariance, spawnChance;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] Vector2Int resolution;
+    [SerializeField] GameObject bubblePrefab;
+    [SerializeField] Transform bubbleParent;
+    [SerializeField] bool autoGenerate, drawGizmos, inverse;
+    //[SerializeField] bool[,] grid;
+
+
+    [ContextMenu("Delete Bubbles")]
+    void DeleteBubbles()
+    {
+        for (int i = bubbleParent.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(bubbleParent.GetChild(i).gameObject);
+        }
+    }
+
+
+    [ContextMenu("Generate Bubbles")]
+    private void GenerateBubbles()
+    {
+        if (Application.isPlaying)
+        {
+            return;
+        }
+
+        for (int i = bubbleParent.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(bubbleParent.GetChild(i).gameObject);
+        }
+
+        //grid = new bool[resolution.x, resolution.y];
+
+        for (int i = 0; i < resolution.x; i++)
+        {
+            for (int j = 0; j < resolution.y; j++)
+            {
+                if (Random.value > spawnChance)
+                {
+                    continue;
+                }
+
+                Vector3 origin = new Vector3(
+                    i * bounds.x / (resolution.x - 1) - bounds.x / 2 + transform.position.x + Random.Range(-offset, offset),
+                    transform.position.z + height,
+                    j * bounds.y / (resolution.y - 1) - bounds.y / 2 + transform.position.z + Random.Range(-offset, offset)
+                );
+                //RaycastHit hit;
+                bool hit = Physics.Raycast(origin, Vector3.down, out _, height, layerMask);
+                if (hit == inverse)
+                {
+                    GameObject bubble = Instantiate(bubblePrefab, bubbleParent);
+                    bubble.transform.localScale = Random.Range(1-sizeVariance, 1+sizeVariance) * scale * Vector3.one;
+                    bubble.transform.position = new Vector3(
+                        origin.x,
+                        transform.position.y + bubble.transform.localScale.y / 2,
+                        origin.z
+                    );
+                }
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (autoGenerate)
+        {
+            GenerateBubbles();
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + new Vector3(0, height / 2, 0), new Vector3(bounds.x, height, bounds.y));
+        if (!drawGizmos)
+        {
+            return;
+        }
+        Gizmos.color = Color.green;
+        for (int i = 0; i < resolution.x; i++)
+        {
+            for (int j = 0; j < resolution.y; j++)
+            {
+                Vector3 origin = new Vector3(
+                    i * bounds.x / (resolution.x-1) - bounds.x / 2 + transform.position.x,
+                    transform.position.z + height,
+                    j * bounds.y / (resolution.y-1) - bounds.y / 2 + transform.position.z
+                );
+
+                Gizmos.DrawRay(origin, Vector3.down * height);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CheckpointHandler.cs
@@ -0,0 +1,176 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class CheckpointHandler : MonoBehaviour
+{
+    List<CheckpointScript>[] checkpoints;
+    [SerializeField] SplineContainer path;
+    [SerializeField] CheckpointScript checkpoint;
+    [SerializeField] int checkpointCount;
+    [SerializeField] float mergeDistance;
+
+
+    [ContextMenu("Delete Checkpoints")]
+    void Delete_Checkpoints()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+
+
+    [ContextMenu("Generate")]
+    void Generate()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+        checkpoints = new List<CheckpointScript>[checkpointCount];
+        if (path == null || checkpoint == null)
+        {
+            return;
+        }
+        for (int i = 0; i < checkpointCount; i++)
+        {
+            float t = i / (float)checkpointCount;
+            var cp = Instantiate(checkpoint, (Vector3)path.Spline.EvaluatePosition(t) + path.transform.position, Quaternion.identity, transform);
+            checkpoints[i] = new List<CheckpointScript> { cp };
+            cp.checkpointIndex = i;
+        }
+        for (int i = 1; i < path.Splines.Count; i++)
+        {
+            float start, end;
+
+            if (!path.KnotLinkCollection.TryGetKnotLinks(new SplineKnotIndex(i, 0), out var links)) continue;
+
+            foreach (var link in links)
+            {
+                Debug.Log(i + " " + link.Knot + " " + link.Spline);
+            }
+
+            var root = links.Where(l => l.Spline == 0);
+
+            if (root.Count() == 0) continue;
+
+            start = path[0].ConvertIndexUnit(root.First().Knot, PathIndexUnit.Knot, PathIndexUnit.Normalized);
+            if (!path.KnotLinkCollection.TryGetKnotLinks(new SplineKnotIndex(i, path.Splines[i].Count - 1), out links)) continue;
+            root = links.Where(l => l.Spline == 0);
+
+            if (root.Count() == 0) continue;
+
+            end = path[0].ConvertIndexUnit(root.First().Knot, PathIndexUnit.Knot, PathIndexUnit.Normalized);
+            for (int j = 0; j < checkpointCount; j++)
+            {
+                float t = j / (float)checkpointCount;
+                if (start < end && (t > start && t < end) || start > end && (t > start && t < 1 || t > 0 && t < end))
+                {
+                    var cp = Instantiate(checkpoint, (Vector3)path.Splines[i].EvaluatePosition((t - start) / (end - start)) + path.transform.position, Quaternion.identity, transform);
+                    checkpoints[j].Add(cp);
+                    cp.checkpointIndex = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < checkpointCount; i++)
+        {
+            for (int j = 0; j < checkpoints[i].Count; j++)
+            {
+                for (int k = j + 1; k < checkpoints[i].Count; k++)
+                {
+                    if (Vector3.Distance(checkpoints[i][j].transform.position, checkpoints[i][k].transform.position) < mergeDistance)
+                    {
+                        checkpoints[i][j].transform.position = (checkpoints[i][j].transform.position + checkpoints[i][k].transform.position) / 2;
+                        DestroyImmediate(checkpoints[i][k].gameObject);
+                        checkpoints[i].RemoveAt(k);
+                        k--;
+                    }
+                }
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        /*for (int i = 0; i < checkpoints.GetLength(0); i++)
+        {
+            for (int j = 0; j < checkpoints[i].Length; j++)
+            {
+                var cp = checkpoints[i][j].GetComponent<CheckpointScript>();
+                cp.checkpointIndex = i;
+            }
+        }*/
+    }
+
+    public int GetNext(int i)
+    {
+        return (i + 1) % checkpoints.Length;
+    }
+
+    public Vector3 GetNearestCheckpoint(int index, Vector3 position)
+    {
+        Vector3 nearest = checkpoints[index][0].transform.position;
+        float minDist = Vector3.Distance(nearest, position);
+        for (int i = 1; i < checkpoints[index].Count(); i++)
+        {
+            float dist = Vector3.Distance(checkpoints[index][i].transform.position, position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearest = checkpoints[index][i].transform.position;
+            }
+        }
+        return nearest;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.red;
+        for (int i = 0; i < checkpointCount; i++)
+        {
+            float t = i / (float)checkpointCount;
+            Gizmos.DrawSphere((Vector3)path.Spline.EvaluatePosition(t) + path.transform.position, 0.1f);
+            Gizmos.DrawWireSphere((Vector3)path.Spline.EvaluatePosition(t) + path.transform.position, mergeDistance);
+        }
+        for (int i = 1; i < path.Splines.Count; i++)
+        {
+            float start, end;
+
+            if (!path.KnotLinkCollection.TryGetKnotLinks(new SplineKnotIndex(i, 0), out var links)) continue;
+
+            foreach (var link in links)
+            {
+                Debug.Log(i + " " + link.Knot + " " + link.Spline);
+            }
+
+            var root = links.Where(l => l.Spline == 0);
+
+            if (root.Count() == 0) continue;
+
+            start = path[0].ConvertIndexUnit(root.First().Knot, PathIndexUnit.Knot, PathIndexUnit.Normalized);
+            if (!path.KnotLinkCollection.TryGetKnotLinks(new SplineKnotIndex(i, path.Splines[i].Count - 1), out links)) continue;
+
+            root = links.Where(l => l.Spline == 0);
+
+            if (root.Count() == 0) continue;
+
+            end = path[0].ConvertIndexUnit(root.First().Knot, PathIndexUnit.Knot, PathIndexUnit.Normalized);
+            for (int j = 0; j < checkpointCount; j++)
+            {
+                float t = j / (float)checkpointCount;
+                if (start < end && (t > start && t < end) || start > end && (t > start && t < 1 || t > 0 && t < end))
+                {
+                    Gizmos.DrawSphere((Vector3)path.Splines[i].EvaluatePosition((t - start) / (end - start)) + path.transform.position, 0.1f);
+                    Gizmos.DrawWireSphere((Vector3)path.Splines[i].EvaluatePosition((t - start) / (end - start)) + path.transform.position, mergeDistance);
+                }
+            }
+        }
+    }
+}
